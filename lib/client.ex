631ac47de120cd11215e9e7c97775558c914ee73
bnns <omethyn@gmail.com>
2017-08-04 17:45:51 -0500

    add more methods to Client

--- lib/client.ex
+++ lib/client.ex
@@ -1,6 +1,6 @@
 defmodule ExPhaxio.Client do
   @moduledoc """
-  Module to interact with Phaxio and send faxes.
+  Module to interact with the Phaxio API.
 
   ## Configuration
 
@@ -25,11 +25,11 @@ defmodule ExPhaxio.Client do
         use ExPhaxio.Client
       end
 
-  The `UserFaxer` module now has `send_fax/2` and other useful methods.
+  The `UserFaxer` module now has `create_fax/2` and other useful methods.
 
   ## Sending a Fax
 
-  Invoke `send_fax/2` method with keyword list of `:to`, `:file`, `:content_url`, etc. For all params, see ExPhaxio.Requests.FaxRequest.
+  Invoke `create_fax/2` method with keyword list of `:to`, `:file`, `:content_url`, etc. For all params, see ExPhaxio.Requests.FaxRequest.
 
     # lib/user_faxer.ex
     defmodule MyApp.UserFaxer do
@@ -37,31 +37,62 @@ defmodule ExPhaxio.Client do
     end
 
     $ iex -S mix
-    iex> MyApp.UserFaxer.send_fax(to: "+15554443333", file: "/path/to/fax.html")
+    iex> MyApp.UserFaxer.create_fax(to: "+15554443333", file: "/path/to/fax.html")
   """
 
   require Logger
   alias ExPhaxio.Requests.FaxRequest
+  alias HTTPoison.Response
 
   defmacro __using__(_config) do
     quote do
-      def send_fax(opts) do
+      def create_fax(opts) do
         fax_request = FaxRequest.new_fax(opts)
-        unquote(__MODULE__).send_fax(fax_request)
+        unquote(__MODULE__).create_fax(fax_request)
+      end
+
+      def cancel_fax(opts) do
+        unquote(__MODULE__).cancel_fax(opts)
+      end
+
+      def show_fax(opts) do
+        unquote(__MODULE__).show_fax(opts)
+      end
+
+      def list_faxes(opts) do
+        unquote(__MODULE__).list_faxes(opts)
+      end
+
+      def resend_fax(opts) do
+        unquote(__MODULE__).resend_fax(opts)
       end
     end
   end
 
-  def send_fax(%FaxRequest{} = request) do
-    with {:ok, response} <- ExPhaxio.create(request) do
-      Logger.info """
-      Sending fax with ExPhaxio:
+  def cancel_fax(id: id), do: ExPhaxio.cancel(id)
 
-      #{inspect request, limit: :infinity}
-      """
-      response
-      |> Map.get(:body)
-      |> Poison.decode
+  def resend_fax(id: id, url: url), do: ExPhaxio.resend(id, url)
+
+  def list_faxes(opts), do: ExPhaxio.list(opts)
+
+  def show_fax(id: id), do: ExPhaxio.show(id)
+
+  def create_fax(%FaxRequest{} = request) do
+    Logger.info """
+    Sending fax with ExPhaxio:
+
+    #{inspect request, limit: :infinity}
+    """
+    with {:ok, response} <- ExPhaxio.create(request),
+         {:ok, %{"success" => true} = body} <- parse_body(response) do
+      {:ok, body}
+    else
+      {:ok, %{"message" => message} = _body} ->
+        {:error, "#{message}"}
+      err ->
+        {:error, "#{inspect err}"}
     end
   end
+
+  defp parse_body(%Response{} = r), do: r |> Map.get(:body) |> Poison.decode
 end
--- test/ex_phaxio_test.exs
+++ test/ex_phaxio_test.exs
@@ -4,16 +4,27 @@ defmodule ExPhaxioTest do
   @test_number "+15005550006"
   @test_file_path "test/test.html"
 
+  defmacro assert_functions(module, funcs \\ []) do
+    quote do
+      assert unquote(funcs)
+      |> Enum.all?(&Enum.member?(unquote(module).__info__(:functions), &1))
+    end
+  end
+
   test "faxers can use Client to automate configuration" do
     defmodule Faxer do
       use ExPhaxio.Client
     end
 
-    assert :functions
-    |> Faxer.__info__
-    |> Enum.member?({:send_fax, 1})
+    assert_functions Faxer, [
+      create_fax: 1,
+      cancel_fax: 1,
+      show_fax: 1,
+      list_faxes: 1,
+      resend_fax: 1
+    ]
 
-    {:ok, response} = Faxer.send_fax(to: @test_number, file: @test_file_path)
+    {_, response} = Faxer.create_fax(to: @test_number, file: @test_file_path)
 
     assert response != nil
   end
