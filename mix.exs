953d04c3ce2b065b2761a245ed6275097d548fdd
Dennis Yao <omethyn@gmail.com>
2019-03-17 17:41:47 -0500

    0.2.3

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExPhaxio.Mixfile do
       name: "ExPhaxio",
       source_url: "https://github.com/bnns/ex_phaxio",
       description: "A simple Phaxio client.",
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
